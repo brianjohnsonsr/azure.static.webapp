3fcdfe9c06764d170429ce50d00daf40a8f90ac5
b.johnson <b.johnson@fan-gene.com>
2021-08-08 19:58:18 -0400

    initial commit

---
+++ pages/test.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout align-center wrap>
+    <v-flex text-center>
+      <v-btn elevation="2" @click="onClick">Click Me</v-btn>
+    </v-flex>
+    <v-flex text-center>
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  methods: {
+    onClick() {
+      console.log('CLICK')
+    }
+  }
+})
+</script>
